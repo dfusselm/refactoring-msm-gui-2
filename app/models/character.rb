a498a77b2d2691d340491b01b85530b5d2e57478
dfusselm <165109847+dfusselm@users.noreply.github.com>
2024-04-19 05:21:52 +0000

    complete

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,13 +10,27 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
-  def movie
-    key = self.movie_id
+  belongs_to(:movie )
+  
+  #def movie
+    #key = self.movie_id
 
-    matching_set = Movie.where({ :id => key })
+    #matching_set = Movie.where({ :id => key })
 
-    the_one = matching_set.at(0)
+    #the_one = matching_set.at(0)
 
-    return the_one
-  end
+    #return the_one
+  #end
+
+  belongs_to(:actor )
+
+  #def actor
+    #key = self.actor_id
+
+    #matching_set = Actor.where({ :id => key })
+
+    #the_one = matching_set.at(0)
+
+    #return the_one
+  #end
 end
